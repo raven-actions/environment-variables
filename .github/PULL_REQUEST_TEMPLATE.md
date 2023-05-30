4325e5d181121935b60a68a1d04bbe557c37cdd4
Dariusz Porowski <3431813+DariuszPorowski@users.noreply.github.com>
2023-05-29 19:40:54 -0700

    🎉 feat(project): init

---
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -0,0 +1,27 @@
+## 💌 Description
+
+<!-- Add a more detailed description of the changes if needed. -->
+
+## 🔗 Related issue
+
+<!-- If your PR refers to a related issue, link it here. -->
+Close: #
+
+## 🔄️ Type of change
+
+<!-- Mark with an `x` all the checkboxes that apply (like `[x]`) -->
+
+- [ ] 📝 Examples, docs, tutorials
+- [ ] 🐛 Bug fix (non-breaking change which fixes an issue)
+- [ ] 🥂 Improvement (non-breaking change which improves an existing feature)
+- [ ] ✨ New feature (non-breaking change which adds functionality)
+- [ ] 💥 Breaking change (fix or feature that would cause existing functionality to change)
+- [ ] 🔒 Security fix
+- [ ] ⬆️ Dependencies update
+
+## ✔️ Checklist
+
+<!-- Mark with an `x` all the checkboxes (like `[x]`) -->
+
+- [ ] I've read the [`Code of Conduct`](../blob/main/.github/CODE_OF_CONDUCT.md) document.
+- [ ] I've read the [`Contributing`](../blob/main/.github/CONTRIBUTING.md) guide.
---
+++ tests/input-helper.test.ts
@@ -0,0 +1,55 @@
+import * as utils from '../src/utils'
+import {getGitHubToken, getInputs} from '../src/input-helper'
+
+describe('input-helper', () => {
+  const mockRepo = {name: 'test-owner/test-repo', repo: {owner: 'test-owner', repo: 'test-repo'}}
+  let mockInputs = {} as any
+  jest.spyOn(utils.core, 'getInput').mockImplementation((name: string) => {
+    return mockInputs[name]
+  })
+
+  beforeEach(() => {
+    jest.clearAllMocks()
+    jest.resetModules()
+  })
+
+  afterEach(() => {
+    mockInputs = {}
+  })
+
+  it('should get GitHub token - input supplied', () => {
+    mockInputs['github-token'] = 'test'
+    const token = getGitHubToken()
+    expect(token).toEqual('test')
+  })
+
+  it('should get GitHub token - input no supplied, from env', () => {
+    const token = getGitHubToken()
+    expect(token).toEqual('456def')
+  })
+
+  it('should get inputs - minimal required', () => {
+    mockInputs['environment'] = 'test'
+    const inputs = getInputs(mockRepo)
+    expect(inputs).toEqual({
+      repository: {name: 'test-owner/test-repo', repo: {owner: 'test-owner', repo: 'test-repo'}},
+      outputTo: 'all',
+      envPrefix: '',
+      deployEnvironment: 'test'
+    })
+  })
+
+  it('should get inputs - all', () => {
+    mockInputs['environment'] = 'test'
+    mockInputs['repository'] = 'test/test'
+    mockInputs['output-to'] = 'env'
+    mockInputs['env-prefix'] = 'test'
+    const inputs = getInputs(mockRepo)
+    expect(inputs).toEqual({
+      repository: {name: 'test/test', repo: {owner: 'test', repo: 'test'}},
+      outputTo: 'env',
+      envPrefix: 'test',
+      deployEnvironment: 'test'
+    })
+  })
+})
